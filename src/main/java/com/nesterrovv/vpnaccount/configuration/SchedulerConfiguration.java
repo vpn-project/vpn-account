d01ef647334bdf9a1a7030fdb7aa6fd38273799d
Ivan Nesterov <71551187+nesterrovv@users.noreply.github.com>
2024-01-18 13:43:37 +0700

    codestyle fix

--- src/main/java/com/nesterrovv/vpnaccount/configuration/SchedulerConfiguration.java
+++ src/main/java/com/nesterrovv/vpnaccount/configuration/SchedulerConfiguration.java
@@ -1,11 +1,11 @@
 package com.nesterrovv.vpnaccount.configuration;
 
+import java.util.concurrent.Executors;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import reactor.core.scheduler.Scheduler;
 import reactor.core.scheduler.Schedulers;
-import java.util.concurrent.Executors;
 
 @Configuration
 public class SchedulerConfiguration {
